ff66be4b572c818b3c3807e39f3d1f5608cefcb9
Dilek Baykara <dilekbaykara@Dileks-MBP.lan>
2022-05-28 15:51:27 -0400

    index.html

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 //Score Start//
 let playerScore = 0;
 let computerScore = 0;
+let roundWinner = '';
 
 //Win Lose Messages//
 let playerWinRound = "You won this round!";
@@ -10,43 +11,44 @@ let draw = "Draw, play again.";
 let playerWins = "You won the game!";
 let computerWins = "You lost the game!";
 
-//Player Selection//
+//Define Buttons//
+var rock = document.getElementById("rockBtn")
+var paper = document.getElementById("paperBtn")
+var scissors = document.getElementById("scissorsBtn")
 
-//Computer Play with array//
-function computerPlay() {
-  let weapons = [`rock`, `paper`, `scissors`];
-  let computerSelection = weapons[Math.floor(Math.random() * weapons.length)];
-  return computerSelection;
-}
+//Button Click Event Listener//
+
+rock.addEventListener('click', function() {
+  console.log('this is working');
+});
+
+paper.addEventListener('click', function() {
+    console.log('the js is working');
+});
+
+scissors.addEventListener('click', function() {
+    console.log('this js is working');
+});
 
 //Play Round//
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === "rock" && computerSelection === "scissors")
-    return playerWinRound + " Your score is: " + playerScore++;
-  if (playerSelection === "scissors" && computerSelection === "rock")
-    return computerWinRound + " Computer score is: " + computerScore++;
-  if (playerSelection === "rock" && computerSelection === "rock") return draw;
-  if (playerSelection === "scissors" && computerSelection === "scissors")
-    return draw;
-  if (playerSelection === "paper" && computerSelection === "rock")
-    return playerWinRound +  " Your score is: " + playerScore++;
-  if (playerSelection === "rock" && computerSelection === "paper")
-    return computerWinRound +  " Computer score is: "  + computerScore++;
-  if (playerSelection === "paper" && computerSelection === "paper") return draw;
-  if (playerSelection === "scissors" && computerSelection === "paper")
-    return playerWinRound + " Your score is: " + playerScore++;
-  if (playerSelection === "paper" && computerSelection === "scissors")
-    return computerWinRound +  " Computer score is: " + computerScore++;
-}
+    if (playerSelection === "rock" && computerSelection === "scissors")
+      playerScore++;
+    if (playerSelection === "scissors" && computerSelection === "rock")
+     computerScore++;
+    if (playerSelection === "rock" && computerSelection === "rock") 
+      draw;
+    if (playerSelection === "scissors" && computerSelection === "scissors")
+      draw;
+    if (playerSelection === "paper" && computerSelection === "rock")
+      playerScore++;
+    if (playerSelection === "rock" && computerSelection === "paper")
+      computerScore++;
+    if (playerSelection === "paper" && computerSelection === "paper") 
+    draw;
+    if (playerSelection === "scissors" && computerSelection === "paper")
+      playerScore++;
+    if (playerSelection === "paper" && computerSelection === "scissors")
+      computerScore++;
+  }
 
-
-/* //Function Game//
-function game() {
-for (let i = 0; i < 5; i++) {
-  let playerSelection = window.prompt("Choose your weapon").toLowerCase(5);
-  const computerSelection = computerPlay(5);
-  console.log(playRound(playerSelection, computerSelection));
-  if (playerScore <5 && computerScore <5) {
-      game();
-    } if (playerScore === 5 && computerScore <= 5) return playerWins;
-  else return computerWins; */
