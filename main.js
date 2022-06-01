920116380029810efc5f796a981c079eca683bd2
Dilek Baykara <dilekbaykara@Dileks-MBP.lan>
2022-05-31 23:49:30 -0400

    Updated function

--- main.js
+++ main.js
@@ -17,9 +17,15 @@ var paper = document.getElementById("paperBtn")
 var scissors = document.getElementById("scissorsBtn")
 const playerSign = document.getElementById('playerSign')
 const computerSign = document.getElementById('computerSign')
+const computerScorePoints = document.getElementsByClassName('computerScore')
+const playerScorePoints = document.getElementsByClassName('playerScore')
 
-//Button Click Event Listener//
+//Game Over Function//
+function isGameOver() {
+  return playerScore === 5 || computerScore === 5
+}
 
+//Button Click Event Listener to Display Choice//
 rock.addEventListener('click', function() {
   playerSign.getElementsByClassName('?')[0]
   playerSign.innerHTML = '<img src="./images/Rock.png" alt="Rock"/>'
@@ -35,60 +41,114 @@ scissors.addEventListener('click', function() {
   playerSign.innerHTML = '<img src="./images/Scissors.png" alt="Scissors"/>'
 });
 
-
 //Play Round//
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "scissors")
-      playerScore++;
-    if (playerSelection === "scissors" && computerSelection === "rock")
-     computerScore++;
-    if (playerSelection === "rock" && computerSelection === "rock") 
-      draw;
-    if (playerSelection === "scissors" && computerSelection === "scissors")
-      draw;
-    if (playerSelection === "paper" && computerSelection === "rock")
-      playerScore++;
-    if (playerSelection === "rock" && computerSelection === "paper")
-      computerScore++;
-    if (playerSelection === "paper" && computerSelection === "paper") 
-    draw;
-    if (playerSelection === "scissors" && computerSelection === "paper")
-      playerScore++;
-    if (playerSelection === "paper" && computerSelection === "scissors")
-      computerScore++;
+  if (playerSelection === computerSelection) {
+    roundWinner = 'tie'
   }
-
-  function isGameOver() {
-    return playerScore === 5 || computerScore === 5
+  if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper') ||
+    (playerSelection === 'paper' && computerSelection === 'rock')
+  ) {
+    playerScore++
+    roundWinner = 'player'
   }
+  if (
+    (computerSelection === 'rock' && playerSelection === 'scissors') ||
+    (computerSelection === 'scissors' && playerSelection === 'paper') ||
+    (computerSelection === 'paper' && playerSelection === 'rock')
+  ) {
+    computerScore++
+    roundWinner = 'computer'
+  }
+  updateScoreMessage(roundWinner, playerSelection, computerSelection)
+}
 
   function updateScore() {
     if (roundWinner === 'tie') {
-      scoreInfo.textContent = "It's a tie!"
+      scoreMessage.textContent = "It's a tie!"
     } else if (roundWinner === 'player') {
-      scoreInfo.textContent = 'You won!'
+      scoreMessage.textContent = 'You won!'
     } else if (roundWinner === 'computer') {
-      scoreInfo.textContent = 'You lost!'
+      scoreMessage.textContent = 'You lost!'
     }
 
+//Display choices for computer selection//
+
+computerSign.getElementsByClassName('?')[0]
+      computerSign.innerHTML = '<img src="./images/Paper.png" alt="Paper"/>'
+
+//Random choice for Computer selection//
 function getRandomChoice() {
   let randomNumber = Math.floor(Math.random() * 3)
   switch (randomNumber) {
     case 0:
-      return 'rock'
+      return 'rock' 
     case 1:
       return 'paper'
     case 2:
       return 'scissors'
   }
 }
-
+//Computer selection function//
 const computerSelection = getRandomChoice()
 playRound(playerSelection, computerSelection)
 updateChoices(playerSelection, computerSelection)
 updateScore()
 
+if (isGameOver()) {
+  openEndgameModal()
+  setFinalMessage()
+}
+}
 
-    playerScorePara.textContent = `Player: ${playerScore}`
-    computerScorePara.textContent = `Computer: ${computerScore}`
+function updateChoices(playerSelection, computerSelection) {
+switch (playerSelection) {
+  case 'rock':
+    playerSign.textContent = ''
+    break
+  case 'paper':
+    playerSign.textContent = ''
+    break
+  case 'scissors':
+    playerSign.textContent = ''
+    break
+}
+
+switch (computerSelection) {
+  case 'rock':
+    computerSign.getElementsByClassName('?')[0]
+    computerSign.innerHTML = '<img src="./images/Rock.png" alt="Rock"/>'
+    break
+  case 'paper':
+    computerSign.getElementsByClassName('?')[0]
+    computerSign.innerHTML = '<img src="./images/Paper.png" alt="Paper"/>'
+    break
+  case 'scissors':
+    computerSign.getElementsByClassName('?')[0]
+    computerSign.innerHTML = '<img src="./images/Scissors.png" alt="Scissors"/>'
+    break
+}
+}
+
+//Update Score Message//
+
+function updateScoreMessage(winner, playerSelection, computerSelection) {
+  if (winner === 'player') {
+    scoreMessage.textContent = `${capitalizeFirstLetter(
+      playerSelection
+    )} beats ${computerSelection.toLowerCase()}`
+    return
   }
+  if (winner === 'computer') {
+    scoreMessage.textContent = `${capitalizeFirstLetter(
+      playerSelection
+    )} is beaten by ${computerSelection.toLowerCase()}`
+    return
+  }
+
+  scoreMessage.textContent = `${capitalizeFirstLetter(
+    playerSelection
+  )} ties with ${computerSelection.toLowerCase()}`
+}
